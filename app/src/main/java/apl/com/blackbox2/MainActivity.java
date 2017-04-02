a9e39e566c393288846a272df6022bb1aef2a8dd
A.P. Liang <apandaliang@gmail.com>
2017-04-02 11:59:53 +0800

    Initial commit. Test?

---
+++ app/src/main/java/apl/com/blackbox2/MainActivity.java
@@ -0,0 +1,14 @@
+package apl.com.blackbox2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Hello World!");
+    }
+}
