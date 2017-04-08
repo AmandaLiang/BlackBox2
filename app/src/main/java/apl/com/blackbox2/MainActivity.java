6714e1f1535790f700c2c036ad638441b94b7aa5
Pax <kristelfung@gmail.com>
2017-04-08 14:21:00 +0800

    SUCCESSFULLY REQUESTS LOCATION!!! STILL HAVENT FIGURED OUT HOW TO LOG IT THOUGH

--- app/src/main/java/apl/com/blackbox2/MainActivity.java
+++ app/src/main/java/apl/com/blackbox2/MainActivity.java
@@ -1,29 +1,26 @@
 package apl.com.blackbox2;
 
-import android.Manifest;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks;
-import com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener;
-import com.google.android.gms.location.LocationServices;
-
-import android.app.Activity;
-import android.location.Location;
 import android.os.Bundle;
-import android.widget.TextView;
-import android.widget.Toast;
 
+import android.util.Log;
+import android.Manifest;
+import android.os.Build;
+import android.content.pm.PackageManager;
 import android.location.Location;
+import android.content.Intent;
+import android.provider.Settings;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.widget.TextView;
+import android.support.v4.app.ActivityCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
+    private TextView t;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,10 +29,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-
+                t.append("\n " + location.getLongitude() + " " + location.getLatitude());
             }
 
             @Override
@@ -50,9 +48,19 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onProviderDisabled(String provider) {
-
+                Intent i = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                startActivity(i);
             }
         };
+
+        // ask permission for location!
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.INTERNET}
+                        ,10);
+            }
+            return;
+        }
 
         // provider, time (500 ms so 0.5s), movement from prev location (1 m)
         locationManager.requestLocationUpdates("gps", 500, 1, locationListener);
